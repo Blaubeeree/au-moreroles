ec509a0ae3d24c33d8bb888e8a573b5d5c94a1b6
Blaubeeree <alederer16@gmail.com>
2021-04-28 11:53:33 +0200

    rename project

---
+++ lua/autorun/moreroles_init.lua
@@ -0,0 +1,32 @@
+﻿hook.Add("OnGamemodeLoaded", "InitMoreRoles", function()
+  if engine.ActiveGamemode() ~= "amongus" then
+    error("The current gamemode is not among us!")
+  end
+
+  -- all scripts should be located in lua/moreroles
+  if SERVER then
+    -- add all clientside and shared scripts
+    AddCSLuaFile("moreroles/cl_gm_overwrites.lua")
+    AddCSLuaFile("moreroles/cl_hud_overwrites.lua")
+    AddCSLuaFile("moreroles/cl_roleselection.lua")
+    AddCSLuaFile("moreroles/sh_gm_overwrites.lua")
+    AddCSLuaFile("moreroles/sh_player_ext.lua")
+    AddCSLuaFile("moreroles/sh_role_module.lua")
+    AddCSLuaFile("includes/modules/roles.lua")
+    -- include all serverside scripts
+    include("moreroles/sv_gm_overwrites.lua")
+    include("moreroles/sv_roleselection.lua")
+    -- add all resources if we need any
+    -- resource.AddWorkshop("cooleWorkshopID")
+  else
+    -- include all clientside scripts
+    include("moreroles/cl_gm_overwrites.lua")
+    include("moreroles/cl_hud_overwrites.lua")
+    include("moreroles/cl_roleselection.lua")
+  end
+
+  -- include all shared scripts
+  include("moreroles/sh_gm_overwrites.lua")
+  include("moreroles/sh_player_ext.lua")
+  include("moreroles/sh_role_module.lua")
+end)
