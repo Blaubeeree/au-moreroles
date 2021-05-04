95854406f95ab6edf616e8cd59b6ac4510ba5b25
Blaubeeree <alederer16@gmail.com>
2021-05-04 17:21:40 +0200

    add workshop id

--- lua/autorun/moreroles_init.lua
+++ lua/autorun/moreroles_init.lua
@@ -3,9 +3,7 @@
     error("The current gamemode is not among us!")
   end
 
-  -- all scripts should be located in lua/moreroles
   if SERVER then
-    -- add all clientside and shared scripts
     AddCSLuaFile("moreroles/cl_gm_overwrites.lua")
     AddCSLuaFile("moreroles/cl_hud_overwrites.lua")
     AddCSLuaFile("moreroles/cl_render_overwrites.lua")
@@ -14,20 +12,16 @@
     AddCSLuaFile("moreroles/sh_player_ext.lua")
     AddCSLuaFile("moreroles/sh_role_module.lua")
     AddCSLuaFile("includes/modules/roles.lua")
-    -- include all serverside scripts
     include("moreroles/sv_gm_overwrites.lua")
     include("moreroles/sv_roleselection.lua")
-    -- add all resources if we need any
-    -- resource.AddWorkshop("cooleWorkshopID")
+    resource.AddWorkshop("2476816620")
   else
-    -- include all clientside scripts
     include("moreroles/cl_gm_overwrites.lua")
     include("moreroles/cl_hud_overwrites.lua")
     include("moreroles/cl_render_overwrites.lua")
     include("moreroles/cl_roleselection.lua")
   end
 
-  -- include all shared scripts
   include("moreroles/sh_gm_overwrites.lua")
   include("moreroles/sh_player_ext.lua")
   include("moreroles/sh_role_module.lua")
